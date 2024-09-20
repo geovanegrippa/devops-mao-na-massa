f4bccca84c5233122fe8cd4580429b2dcda72c15
Geovane José Grippa <geovane.grippa@gmail.com>
2024-09-20 19:33:29 -0300

    Todos meus arquivos

---
+++ lab-prometheus/provision.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+cd /etc/yum.repos.d/
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo yum update -y >/dev/null 2>&1
+sudo yum install -y epel-release
+sudo yum install -y yum-utils
+sudo yum install -y telnet
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli  containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
---
+++ lab-swarm/provision.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+cd /etc/yum.repos.d/
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sudo yum update -y >/dev/null 2>&1
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli  containerd.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
